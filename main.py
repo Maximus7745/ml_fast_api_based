6a681420e680571250839f061fafecab8144a6b5
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-05-30 15:26:13 +0500

    add method info
    Добавление метода info

--- main.py
+++ main.py
@@ -21,3 +21,12 @@ def root():
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
+
+
+@app.get("/info/")
+def info():
+    return {
+        "model_name": classifier.model.config.name_or_path,
+        "framework": "transformers",
+        "version": transformers_version
+    }
